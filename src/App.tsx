1caaf9a5e0f74d9f6286fb7736d212562dc06a0b
Mujapira <malmasmello2@gmail.com>
2022-08-24 20:18:41 -0300

    feat: complete a task flow

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import styles from "./App.module.css"
 import { PlusCircle, Trash } from 'phosphor-react'
 import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react'
 import clipboard from './assets/clipboard.svg'
+import done from './assets/done.svg'
 
 
 interface Task {
   id: string;
   content: string;
   completed: boolean;
+  publishedAt: Date,
 }
 
 function App() {
   const [tasks, setTasks] = useState<Task[]>([])
   const [newTaskText, setNewTaskText] = useState('')
+  const [completedTasks, setCompletedTasks] = useState<number>(0)
 
   function handleCreateNewTask(e: FormEvent) {
     e.preventDefault()
@@ -23,7 +26,8 @@ function App() {
     const newTask: Task = {
       id,
       content: newTaskText,
-      completed: false
+      completed: false,
+      publishedAt: new Date(),
     }
 
     setTasks([...tasks, newTask])
@@ -49,11 +53,17 @@ function App() {
     const filteredTasks = tasks.filter(task => task.id !== id);
     const task = tasks.filter(task => task.id === id);
     const updatedTask = task[0]
-    updatedTask.completed = true;
+
+    if (updatedTask.completed === false) {
+      updatedTask.completed = true
+      setCompletedTasks(prevState => prevState + 1)
+    } else {
+      return
+    }
 
     setTasks([...filteredTasks, updatedTask])
   }
-  
+
   const isNewTaskEmpty = newTaskText.length === 0;
   console.log(tasks)
 
@@ -79,8 +89,13 @@ function App() {
         </div>
         <div className={styles.taskList}>
           <header className={styles.wrapper}>
-            <span className={styles.created}>Tarefas criadas <span>0</span></span>
-            <span className={styles.finished}>Concluídas <span>0</span></span>
+            <span className={styles.created}>Tarefas criadas <span>{tasks.length}</span></span>
+            {tasks.length >= 1
+              ?
+              <span className={styles.finished}>Concluídas <span>{completedTasks} de {tasks.length}</span></span>
+              :
+              <span className={styles.finished}>Concluídas <span>{tasks.length}</span></span>
+            }
           </header>
 
           {tasks.length === 0
@@ -99,7 +114,7 @@ function App() {
               return (
                 <main key={task.id} className={styles.main}>
                   <div className={styles.task}>
-                    {task.completed
+                    {task.completed === false
                       ?
                       <>
                         <button className={styles.finishTask} onClick={() => handleFinishTask(task.id)}></button>
@@ -108,8 +123,8 @@ function App() {
                       </>
                       :
                       <>
-                        <button className={styles.finishTask} onClick={() => handleFinishTask(task.id)}></button>
-                        <span className={styles.taskContent}>{task.content}</span>
+                        <button className={styles.finishTaskDone} onClick={() => handleFinishTask(task.id)}> <img src={done} alt="" /> </button>
+                        <span className={styles.taskContentDone}>{task.content}</span>
                         <button className={styles.deleteTask} onClick={() => handleDeleteTask(task.id)}><Trash size={16} /></button>
                       </>
                     }
